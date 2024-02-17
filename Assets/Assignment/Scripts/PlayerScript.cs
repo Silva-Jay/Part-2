2073fcc2dd9cd05bf350f0b929a21107315a10e9
Sophia Grasman <grasman@shernet.sheridancollege.ca>
2024-02-17 01:07:36 -0500

    Player assets, code, and animations created and implemented

---
+++ Assets/Assignment/Scripts/PlayerScript.cs
@@ -0,0 +1,62 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //movement vectors
+    Vector2 direction;
+    Vector2 destination;
+
+    //rigidbody component
+    Rigidbody2D rigidbody;
+
+    //movement speed
+    public float speed = 7f;
+
+    //animator component
+    Animator silva;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        silva = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        //direction vector is set to the direction where the mouse was clicked
+        direction = destination - (Vector2)transform.position;
+
+        //stop player movement when it hits the destination
+        if (direction.magnitude < 0.1)
+        {
+                direction = Vector2.zero;
+        }
+
+        //increment player
+        rigidbody.MovePosition(rigidbody.position + direction.normalized * speed * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if mouse is pressed, set destination vector to where it was pressed on the screen
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        //if player moves, set floats in animation controller to the proper direction and speed
+        if (direction.x != 0 || direction.y != 0)
+        {
+            silva.SetFloat("Horizontal", direction.x);
+            silva.SetFloat("Vertical", direction.y);
+        }
+
+        silva.SetFloat("Speed", direction.sqrMagnitude);
+
+    }
+}
